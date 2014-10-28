ef2aec88dfe483a4c9a14079ca1812629713e755
Todd Mannherz <todd.mannherz@gmail.com>
2014-10-28 13:44:22 -0600

    SD Release branch restored

--- projects/Releasesafedinarcom.php
+++ projects/Releasesafedinarcom.php
@@ -4,5 +4,5 @@
  * Define custom project type to use.
  */
 define('DEPLOY_PROJECT_TYPE', 'magento');
-define('DEPLOY_BRANCH', 'MC-11');
+define('DEPLOY_BRANCH', 'master');
 define('DEPLOY_PROJECT_ENV', 'release');
